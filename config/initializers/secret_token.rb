9489b5d166ff42f73371e92cbb3a09b2d260c438
Santiago Angee <sangeeag@gmail.com>
2014-02-16 21:20:57 -0500

    doing a commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reto1Webimagen::Application.config.secret_key_base = '0cd1da9f6ff33b4dae3bdfc85c573c01998189c509f2aeef45ecb1b483ea102c11ac54471b16c676d1e6c92f6af519dcabf3a23d04407cf7cf2e6ef1f9cc5faf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reto1Webimagen::Application.config.session_store :cookie_store, key: '_Reto1-WebImagen_session'
